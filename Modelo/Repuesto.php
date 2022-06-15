bba798d5fcd66c7dd0124341f9c37838e2633e6d
DebbieKristyCalderonMorales <105973446+DebbieKristyCalderonMorales@users.noreply.github.com>
2022-06-15 18:59:05 -0500

    Add files via upload

---
+++ Modelo/Repuesto.php
@@ -0,0 +1,31 @@
+<?php
+
+class Repuesto {
+
+    protected $Nombre;
+    protected $Stock;
+    protected $Precio;
+
+    protected function AgregarRepuesto() {
+        include_once '../Config/Conexion.php';
+        $ic = new Conexion();
+
+        $sql = "INSERT INTO repuesto (nombre,stock,precio) VALUES (?,?,?)";
+        $insertar = $ic->db->prepare($sql);
+        $insertar->bindParam(1, $this->Nombre);
+        $insertar->bindParam(2, $this->Stock);
+        $insertar->bindParam(3, $this->Precio);
+        $insertar->execute();
+    }
+
+    public function ListarRepuesto() {
+        include_once '../Config/Conexion.php';
+        $ic = new Conexion();
+        $sql = "SELECT * FROM repuesto";
+        $mostrar = $ic->db->prepare($sql);
+        $mostrar->execute();
+        $objusuario = $mostrar->fetchAll(PDO::FETCH_OBJ);
+        return $objusuario;
+    }
+
+}
